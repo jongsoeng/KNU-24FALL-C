7155361b5e5c817ba51912e692954d75120bff72
jongsoeng <cjs05david@gmail.com>
2024-09-19 14:11:58 +0900

    Update day03-1.c

--- day03/day03-1.c
+++ day03/day03-1.c
@@ -8,21 +8,21 @@ int main(void) {
 	printf("소수인지를 확인할 수를 입력하세요 : ");
 	scanf_s("%d", &a);
 
-	if (prime_number(a)) {
+	if (prime_number(a))
 		printf("소수에요");
-	}
-	else {
+	else 
 		printf("소수가 아니에요");
-	}
 
 	return 0;
 }
 
 int prime_number(int k) {
-	if (k <= 1) { return 0; }
+	if (k <= 1)
+		return 0;
 
-	for (int i = 2 ; i * i <= k; i++) {
-		if (k % i == 0) { return 0; }
+	for (int i = 2; i * i <= k; i++) {
+		if (k % i == 0)
+			return 0;
 	}
 
 	return 1;
