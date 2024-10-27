062177f5f85bada348a1a853babeaa7c80493e61
jongsoeng <cjs05david@gmail.com>
2024-10-27 14:35:12 +0900

    day09

---
+++ day09/day09-1.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+
+struct v1 {
+    int x1, y1, z1;
+};
+struct v2 {
+    int x2, y2, z2;
+};
+
+void hap(struct v1 V1, struct v2 V2);
+void cha(struct v1 V1, struct v2 V2);
+void out(struct v1 V1, struct v2 V2);
+void in(struct v1 V1, struct v2 V2);
+
+int main(void) {
+    struct v1 V1;
+    struct v2 V2;
+    int x, y, z;
+    int rst;
+    int cmd;
+
+    while (1) {
+        printf("첫번째 벡터 : ");
+        scanf("%d %d %d", &V1.x1, &V1.y1, &V1.z1);
+        printf("두번째 벡터 : ");
+        scanf("%d %d %d", &V2.x2, &V2.y2, &V2.z2);
+        printf("-----------------------\n");
+    
+        printf("1. 벡터의 합\n2. 벡터의 차\n3. 벡터의 외적\n4. 벡터의 내적\n5. 종료\n");
+        printf("-----------------------\n");
+        printf("명령 입력 : ");
+        scanf("%d", &cmd);
+
+        switch (cmd) {
+        case 1:
+            hap(V1, V2);
+            break;
+        case 2:
+            cha(V1, V2);
+            break;
+        case 3:
+            out(V1, V2);
+            break;
+        case 4:
+            in(V1, V2);
+            break;
+        case 5:
+            printf("프로그램을 종료합니다.\n");
+            return 0;
+        default:
+            break;
+        }
+    }
+}
+
+void hap(struct v1 V1, struct v2 V2) {
+    int x = V1.x1 + V2.x2;
+    int y = V1.y1 + V2.y2;
+    int z = V1.z1 + V2.z2;
+
+    printf("벡터의 합은 V1 + V2 = (%d, %d, %d)\n", x, y, z);
+}
+void cha(struct v1 V1, struct v2 V2) {
+    int x = V1.x1 - V2.x2;
+    int y = V1.y1 - V2.y2;
+    int z = V1.z1 - V2.z2;
+
+    printf("벡터의 차는 V1 - V2 = (%d, %d, %d)\n", x, y, z);
+}
+void out(struct v1 V1, struct v2 V2) {
+    int x = V1.x1 * V2.x2;
+    int y = V1.y1 * V2.y2;
+    int z = V1.z1 * V2.z2;
+    int result = x + y + z;
+
+    printf("벡터의 내적은 V1 . V2 = %d\n", result);
+}
+void in(struct v1 V1, struct v2 V2) {
+    int x = (V1.y1 * V2.z2) - (V1.z1 * V2.y2);
+    int y = (V1.z1 * V2.x2) - (V1.x1 * V2.z2);
+    int z = (V1.x1 * V2.y2) - (V1.y1 * V2.x2);
+
+    printf("벡터의 외적은 x = %d, y = %d, z = %d\n", x, y, z);
+}
---
+++ day09/day09-2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+struct PtrInfo {
+    int ID;
+    char name[100];
+    int money;
+};
+
+void printProduct(struct PtrInfo p[5]);
+
+int main(void) {
+    struct PtrInfo *p;
+    int a;
+    
+    printProduct(p);
+
+    return 0;
+}
+
+void printProduct(struct PtrInfo p[5]) {
+    int index = 0;
+
+    for (int i = 0; i < 5; i++) {
+        printf("상품 정보를 입력하세요 (입력 중단은 ID에 0입력)\n");
+        printf("상품 ID : ");
+        scanf("%d", &p[i].ID);
+        if (p[i].ID == 0) {
+            break;
+        }
+        printf("상품명 : ");
+        scanf("%s", &p[i].name);
+        printf("가격 : ");
+        scanf("%d", &p[i].money);
+
+        index++;
+    }
+
+    printf("<<입력된 상품 목록>>\n");
+    for (int j = 0; j < index; j++) {
+        printf("상품 ID : %d\n상품명 : %s\n가격 : %d\n\n", p[j].ID, p[j].name, p[j].money);
+    }
+}
